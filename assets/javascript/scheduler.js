fff74af3c88cf46f9c1539b5564e241b53740cd1
Guillaume Paugam <guipaugam@icloud.com>
2018-03-30 16:23:30 -0500

    Linking the resources in the HTML

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCd2mhH25hb1RePzFqIWCC87v7B1D4f3nY",
+    authDomain: "scheduler-9c1dc.firebaseapp.com",
+    databaseURL: "https://scheduler-9c1dc.firebaseio.com",
+    projectId: "scheduler-9c1dc",
+    storageBucket: "scheduler-9c1dc.appspot.com",
+    messagingSenderId: "276460451943"
+  };
+  firebase.initializeApp(config);
+
+  
